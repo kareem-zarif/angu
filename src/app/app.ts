ad2a86a12bfd76ab7111d6f9c52bae2f16184c3f
MahmoudAbdulatif <mahmoudabdullitif@gmail.com>
2025-08-01 17:34:05 +0300

    Auth_Login&Register

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { AboutUs } from "./components/about-us/about-us";
+import { RegisterSelection } from "./components/register-selection/register-selection";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, AboutUs, RegisterSelection],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
